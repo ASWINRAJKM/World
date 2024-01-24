8b37cd6f118452b6a299fe656015bf619f38c3a6
ASWINRAJKM <157572920+ASWINRAJKM@users.noreply.github.com>
2024-01-24 12:54:23 +0530

    Update StatesDto.cs
    Reverted back to original version

--- WorldAPI/DTO/States/StatesDto.cs
+++ WorldAPI/DTO/States/StatesDto.cs
@@ -2,7 +2,7 @@
 {
     public class StatesDto
     {
-      
+        public int Id { get; set; }
         public string Name { get; set; }
         public double Population { get; set; }
         public int CountryId { get; set; }
